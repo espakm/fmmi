0495aa354cca0187f116ef829eecd3067f3bfcfd
Miklos Espak <miklos@smartcow.ai>
2021-09-06 21:25:01 +0200

    Simplify test

--- test/fmmi/fmmi_test.cpp
+++ test/fmmi/fmmi_test.cpp
@@ -6,49 +6,44 @@ using namespace fmmi;
 
 TEST_CASE("fmmi mul_fast")
 {
-    matrix<2, 2> mx1{
-        -2.0, 3.0,
-        -7.0, 9.0,
+    matrix<4, 6> mx1{
+        1.0, 2.0, 3.0, 4.0, 5.0, 6.0,
+        7.0, 8.0, 9.0, 10.0, 11.0, 12.0,
+        13.0, 14.0, 15.0, 16.0, 17.0, 18.0,
+        19.0, 20.0, 21.0, 22.0, 23.0, 24.0,
     };
-
-    matrix<2, 2> mx2{
-        5.0, -8.0,
-        4.0, -6.0,
+    matrix<4, 6> mx2{
+        -1.0, -2.0, -3.0, -4.0, -5.0, -6.0,
+        -7.0, -8.0, -9.0, -10.0, -11.0, -12.0,
+        -13.0, -14.0, -15.0, -16.0, -17.0, -18.0,
+        -19.0, -20.0, -21.0, -22.0, -23.0, -24.0,
     };
 
     SUBCASE("2x2 times 2x2")
     {
-        matrix<2, 2> mx3;
-        mul(mx1, mx2, mx3);
+        const auto a = mx1.partition<0, 0, 2, 2>();
+        const auto b = mx2.partition<0, 0, 2, 2>();
 
-        matrix<2, 2> mx4;
-        mul_fast(mx1, mx2, mx4);
+        matrix<2, 2> c;
+        mul(a, b, c);
 
-        CHECK_EQ(mx3, mx4);
+        matrix<2, 2> d;
+        mul_fast(a, b, d);
+
+        CHECK_EQ(c, d);
     }
-
-    matrix<4, 4> mx5{
-        1.0, 2.0, 3.0, 4.0,
-        7.0, 8.0, 9.0, 10.0,
-        13.0, 14.0, 15.0, 16.0,
-        19.0, 20.0, 21.0, 22.0,
-    };
-
-    matrix<4, 4> mx6{
-        3.0, 4.0, 5.0, 6.0,
-        9.0, 10.0, 11.0, 12.0,
-        15.0, 16.0, 17.0, 18.0,
-        21.0, 22.0, 23.0, 24.0,
-    };
 
     SUBCASE("4x4 times 4x4")
     {
-        matrix<4, 4> mx3;
-        mul(mx5, mx6, mx3);
+        const auto a = mx1.partition<0, 0, 4, 4>();
+        const auto b = mx2.partition<0, 0, 4, 4>();
 
-        matrix<4, 4> mx4;
-        mul_fast(mx5, mx6, mx4);
+        matrix<4, 4> c;
+        mul(a, b, c);
 
-        CHECK_EQ(mx3, mx4);
+        matrix<4, 4> d;
+        mul_fast(a, b, d);
+
+        CHECK_EQ(c, d);
     }
 }
